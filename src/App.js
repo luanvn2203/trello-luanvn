f19c5d340867fcde1584bc744800dac8a08c9cd8
luanvn2203 <59554558+luanvn2203@users.noreply.github.com>
2021-09-26 19:45:33 +0700

    clone FE

--- src/App.js
+++ src/App.js
@@ -1,9 +1,121 @@
-import './App.css';
+
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div className="app-main-container">
+      <nav className="navbar app">App bar</nav>
+      <nav className="navbar board">Board bar</nav>
+      <div className="board-colums">
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://64.media.tumblr.com/d8a2f214a661bf7ab59eaae3534aef61/828d227d23aeb43d-96/s1280x1920/87f6e646b9b9795989d90958eb9b232336d16c4d.jpg"
+                alt="nhutluandev" />
+              Title: Luanvn-dev
+            </li>
+            <li>Add what you like to work on below</li>
+            <li>Add what you like to work on below</li>
+            <li>Add what you like to work on below</li>
+            <li>Add what you like to work on below</li>
+            <li>Add what you like to work on below</li>
+          </ul>
+          <footer>
+            Add another card +
+          </footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://64.media.tumblr.com/d8a2f214a661bf7ab59eaae3534aef61/828d227d23aeb43d-96/s1280x1920/87f6e646b9b9795989d90958eb9b232336d16c4d.jpg"
+                alt="nhutluandev" />
+              Title: Luanvn-dev
+            </li>
+            <li>Add what you like to work on below</li>
+            <li>Add what you like to work on below</li>
+            <li>Add what you like to work on below</li>
+            <li>Add what you like to work on below</li>
+            <li>Add what you like to work on below</li>
+          </ul>
+          <footer>
+            Add another card +
+          </footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://64.media.tumblr.com/d8a2f214a661bf7ab59eaae3534aef61/828d227d23aeb43d-96/s1280x1920/87f6e646b9b9795989d90958eb9b232336d16c4d.jpg"
+                alt="nhutluandev" />
+              Title: Luanvn-dev
+            </li>
+            <li>Add what you like to work on below</li>
+            <li>Add what you like to work on below</li>
+            <li>Add what you like to work on below</li>
+            <li>Add what you like to work on below</li>
+            <li>Add what you like to work on below</li>
+          </ul>
+          <footer>
+            Add another card +
+          </footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://64.media.tumblr.com/d8a2f214a661bf7ab59eaae3534aef61/828d227d23aeb43d-96/s1280x1920/87f6e646b9b9795989d90958eb9b232336d16c4d.jpg"
+                alt="nhutluandev" />
+              Title: Luanvn-dev
+            </li>
+            <li>Add what you like to work on below</li>
+            <li>Add what you like to work on below</li>
+            <li>Add what you like to work on below</li>
+            <li>Add what you like to work on below</li>
+            <li>Add what you like to work on below</li>
+          </ul>
+          <footer>
+            Add another card +
+          </footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://64.media.tumblr.com/d8a2f214a661bf7ab59eaae3534aef61/828d227d23aeb43d-96/s1280x1920/87f6e646b9b9795989d90958eb9b232336d16c4d.jpg"
+                alt="nhutluandev" />
+              Title: Luanvn-dev
+            </li>
+            <li>Add what you like to work on below</li>
+            <li>Add what you like to work on below</li>
+            <li>Add what you like to work on below</li>
+            <li>Add what you like to work on below</li>
+            <li>Add what you like to work on below</li>
+          </ul>
+          <footer>
+            Add another card +
+          </footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://64.media.tumblr.com/d8a2f214a661bf7ab59eaae3534aef61/828d227d23aeb43d-96/s1280x1920/87f6e646b9b9795989d90958eb9b232336d16c4d.jpg"
+                alt="nhutluandev" />
+              Title: Luanvn-dev
+            </li>
+            <li>Add what you like to work on below</li>
+            <li>Add what you like to work on below</li>
+            <li>Add what you like to work on below</li>
+            <li>Add what you like to work on below</li>
+            <li>Add what you like to work on below</li>
+          </ul>
+          <footer>
+            Add another card +
+          </footer>
+        </div>
+      </div>
     </div>
   );
 }
